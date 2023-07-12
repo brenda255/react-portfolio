b80afde3f0e274e412b4eee16d5c481db0eee54e
brenda <115198624+brenda255@users.noreply.github.com>
2023-07-12 13:56:50 -0600

    added to resume file,greeting, css

--- src/components/pages/Greeting.js
+++ src/components/pages/Greeting.js
@@ -5,10 +5,11 @@ import image from '../../images/me.jpg';
 export default function Greeting() {
   return (
     <div>
-    <img className="circle-image"  src={image} alt='me'/>
-    <div className='greeting'>
-            <h1>Welcome to my portfolio! Crafting innovative web experiences with passion and precision.</h1>
-    </div>
+      <img className="circle-image" src={image} alt='me' />
+      <div className='greeting'>
+        <h1 className="greeting-one">Welcome to my portfolio!</h1>
+        <h1> Crafting innovative web experiences with passion and precision.</h1>
+      </div>
     </div>
   );
 }
--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -8,7 +8,7 @@ export default function Resume() {
       <p>
         Download my <a href="https://docs.google.com/document/d/1uuReNEPYVU-HyOk8_85ByvHY04RgJ0qHXTcmk_wJPw4/edit?usp=sharing">Resume</a>
 </p>
-<h2>Front-end Proficiencies</h2>
+{/* <h2>Front-end Proficiencies</h2>
 <ul>
   <li>HTML</li>
   <li>CSS</li>
@@ -27,7 +27,23 @@ export default function Resume() {
   <li>MongoDB, Mongoose</li>
   <li>Rest</li>
   <li>GraphQL</li>
+</ul> */}
+
+<h1>Languages, Skills, and Libraries</h1>
+<div className='skills-container'>
+<ul>
+  <li><img src='https://skillicons.dev/icons?i=html' title='html' alt='html'/></li>
+  <li><img src='https://skillicons.dev/icons?i=css' title='css' alt='css'/></li>
+  <li><img src='https://skillicons.dev/icons?i=js' title='js' alt='js'/></li>
+  <li><img src='https://skillicons.dev/icons?i=react' title='react' alt='react'/></li>
+  <li><img src='https://skillicons.dev/icons?i=jquery' title='jquery' alt='jquery'/></li>
+  <li><img src='https://skillicons.dev/icons?i=nodejs' title='nodejs' alt='nodejs'/></li>
+  <li><img src='https://skillicons.dev/icons?i=mongodb' title='mongodb' alt='mongodb'/></li>
+  <li><img src='https://skillicons.dev/icons?i=mysql' title='mysql' alt='mysql'/></li>
+  <li><img src='https://skillicons.dev/icons?i=github' title='github' alt='github'/></li>
 </ul>
+
+    </div>
     </div>
   );
 }
