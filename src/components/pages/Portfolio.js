7468460a7ac79f216943eae2067276daef496568
brenda <115198624+brenda255@users.noreply.github.com>
2023-07-12 11:31:19 -0600

    added info portfolio pg, css

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -5,21 +5,16 @@ import streaming from '../../images/streaming.png';
 import gourmet from '../../images/gourmet.png';
 import ollisander from '../../images/ollisander.png';
 import weather from '../../images/weather.png';
-// import quiz from '../../images/quiz.jpg';
-// import password from '../../images/passwrd.jpg';
 
 export default function Portfolio() {
   return (
     <div className='portfolio'> 
-      <h1>Portfolio</h1>
+      <h1>My Portfolio</h1>
       <div className='pics'>
-        <a href="https://sifrult.github.io/streaming_locator/"><img alt="streaming" src={streaming}></img>  </a><br></br>
-        <a href="https://salty-oasis-46016.herokuapp.com/"><img alt="gourmet" src={gourmet}></img>  </a><br></br>
-        <a href="https://wizard-wands-production.up.railway.app/"><img alt="ollisander" src={ollisander}></img>  </a><br></br>
-        <a href="https://brenda255.github.io/weather-dashboard/"><img alt="weather" src={weather}></img>  </a><br></br>
-        {/* <a href="https://brenda255.github.io/coding-quiz/"><img alt="quiz" src={quiz}></img>  </a><br></br><br></br>
-        <a href="https://brenda255.github.io/password-generator/"><img alt="password" src={password}></img>  </a><br></br> */}
-
+        <a  href="https://sifrult.github.io/streaming_locator/"><img alt="streaming" className="portfolio-img" src={streaming}></img>  </a><br></br>
+        <a  href="https://salty-oasis-46016.herokuapp.com/"><img alt="gourmet" className="portfolio-img" src={gourmet}></img>  </a><br></br>
+        <a  href="https://wizard-wands-production.up.railway.app/"><img alt="ollisander" className="portfolio-img" src={ollisander}></img>  </a><br></br>
+        <a  href="https://brenda255.github.io/weather-dashboard/"><img alt="weather" className="portfolio-img" src={weather}></img>  </a><br></br>
       </div>
       
       
